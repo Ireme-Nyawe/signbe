be6bb4228995efba9eaf6ed0b020318296e80484
Ireme-Nyawe <akimanaj41@gmail.com>
2024-06-01 20:48:35 +0200

    create google auth controller

---
+++ src/controller/googleAth.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import users from "../models/user.js";
+const googleAuth = async (req, res) => {
+  try {
+    const logingUser = await users.findOne({
+      email: req.user.email,
+    });
+    if (logingUser) {
+      return res.redirect(
+        `${process.env.FRONTEND_URL}/login?token=${generateToken(
+          logingUser._id
+        )}`
+      );
+    } else {
+      const user = await users.create({
+        email: req.user.email,
+        password: "11111",
+      });
+      return res.redirect(
+        `${process.env.FRONTEND_URL}/login?token=${generateToken(user._id)}`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+export default googleAuth;
