6f95369bfd28d1c67ace49f0e52b24508acd49d1
Ireme-Nyawe <akimanaj41@gmail.com>
2024-06-01 20:23:20 +0200

    user signin

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,19 +1,47 @@
+import Jwt from "jsonwebtoken";
 import users from "../models/user.js";
-export const signUp = async (req,res)=>{
-try {
-    const data= req.body;
-    console.log(data)
-const user= await users.create(data);
-if(user){
-    return res.status(201).json({
-        success:true,
-        user:user
-    })
-}
-} catch (error) {
+
+export const signUp = async (req, res) => {
+  try {
+    const data = req.body;
+    const user = await users.create(data);
+    if (user) {
+      return res.status(201).json({
+        success: true,
+        user: user,
+      });
+    }
+  } catch (error) {
     return res.status(400).json({
-        success:false,
-        error:error
-    })
-}
-}
+      success: false,
+      error: error,
+    });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const login = await users.findOne({ email: email, password: password });
+    if (login) {
+      const token = await Jwt.sign({id:login._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.EXPIRE_TIME,
+      });
+      return res.status(200).json({
+        success: true,
+        user: login,
+        token: token,
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "User Not Found",
+      });
+    }
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e,
+    });
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signUp } from "../controller/user.js";
+import { signIn, signUp } from "../controller/user.js";
 const routeUser = express.Router();
 routeUser.post("/signup", signUp);
+routeUser.post("/signin",signIn)
 export default routeUser;
