dd1abfbb670a21fcab2ca20cdf3ddf898a770e28
Ireme-Nyawe <akimana.inono@gmail.com>
2024-06-02 11:40:57 +0200

    correct auth routes

--- src/routes/googleAuth.js
+++ src/routes/googleAuth.js
@@ -3,20 +3,18 @@ import passport from "passport";
 import googleAuth from "../controller/googleAuth.js";
 import "../config/passport.js"; 
 const googleRoute = express.Router();
+googleRoute.get('/google',passport.authenticate('google', { scope: ['email', 'profile'] }));
 googleRoute.get(
-  `/google`,
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
-googleRoute.get(
-  `/google/callback`,
+  "/google/callback",
   passport.authenticate("google", {
-    successRedirect: "/protected",
-    failureRedirect: "/failure",
+    successRedirect: "/google/protected",
+    failureRedirect: "/google/failure",
   })
 );
-googleRoute.get('/protected',googleAuth);
-googleRoute.get('/failure',(req,res)=>{
+googleRoute.get("/google/protected",googleAuth);
+googleRoute.get("/google/failure",(req,res)=>{
 res.send("Authentication Failed!");
 })
 
 export default googleRoute;
+
